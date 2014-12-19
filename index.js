1ef9269f3d2daa67212500476ab77b7bf0b2bffa
Andrew Reed <w.andrew.reed@gmail.com>
2014-12-19 15:26:50 +0700

    hexify and decify conversion functions

---
+++ index.js
@@ -0,0 +1,219 @@
+var Big = require('big.js');
+var _ = require('highland');
+
+/**
+ * Pads the beginning of a string to reach the requried length.
+ * @param {string} p - the character to use to pad
+ * @param {number} l - the length of the padded string
+ * @param {string} h - the string to pad
+ */
+function pad(p, l, h) {
+  //the number of characters of padding required
+  return (new Array(l - h.length + 1)).join(p) + h;
+}
+
+exports.pad = pad;
+
+/**
+ * Curried pad function using '0' for the pad character.
+ * @function
+ * @param {number} l
+ * @param {string} h
+ */
+var pad0 = exports.pad0 = _.curry(pad, '0');
+
+/**
+ * pad0 with length of 2
+ * @function
+ * @param {string} h
+ */
+exports.pad0Byte = pad0(2);
+
+/**
+ * pad0 with length of 4
+ * @function
+ * @param {string} h
+ */
+exports.pad0Wyde = pad0(4);
+
+/**
+ * pad0 with length of 8
+ * @function
+ * @param {string} h
+ */
+exports.pad0Tetra = pad0(8);
+
+/**
+ * pad0 with length of 16
+ * @function
+ * @param {string} h
+ */
+exports.pad0Octa = pad0(16);
+
+/**
+ * pad0 with a length of 32.
+ * @function
+ * @param {string} h
+ */
+exports.pad0Sexa = pad0(32);
+
+/**
+ * curried pad function using 'F' as the pad character.
+ * @function
+ * @param {number} l
+ * @param {string} h
+ */
+var padF = exports.padF = _.curry(pad, 'F');
+
+/**
+ * padF with length of 2 for 8 bit numbers.
+ * @function
+ * @param {string} h
+ */
+exports.padFByte = padF(2);
+
+/**
+ * padF with length of 4 for 16 bit numbers.
+ * @function
+ * @param {string} h
+ */
+exports.padFWyde = padF(4);
+
+/**
+ * padF with length of 8 for 32 bit numbers.
+ * @function
+ * @param {string} h
+ */
+exports.padFTetra = padF(8);
+
+/**
+ * padF with length of 16 for 64 bit numbers.
+ * @function
+ * @param {string} h
+ */
+exports.padFOcta = padF(16);
+
+/**
+ * padF with length of 32 for 128 bit numbers.
+ * @function
+ * @param {string} h
+ */
+exports.padFSexa = padF(32);
+
+/**
+ * Returns the hexadecimal equivalent of a decimal string.
+ * Uses two's-complement encoding for negatives.
+ * @function
+ * @param {string} deci
+ * @param {number} bits - 8, 16, 32, 64, or 128
+ */
+exports.hexify = (function() {
+  var two = Big(2);
+  var n2h = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ];
+
+  return function hexify(deci, bits) {
+    var charLength = bits / 4;
+    var isNegative = deci.charAt(0) === '-';
+    var pad = isNegative ? padF : pad0;
+
+    var digits = [];
+    var q = new Big(deci).mod(two.pow(bits));
+    var r;
+
+    if (isNegative) {
+      //fix Big.js modulus operation
+      q = two.pow(bits).plus(q);
+    }
+
+    while (q.gte(1)) {
+      r = q.mod(16);
+      digits.unshift(r.toString());
+      q = q.div(16).round(0, 0);
+    }
+
+    var hex = digits.map(function(d) {
+      return n2h[d];
+    }).join('') || '0';
+
+    var padded = pad(charLength, hex);
+    if (padded.length > charLength) {
+      return padded.substring(padded.length - charLength);
+    }
+    return padded;
+  };
+})();
+
+/**
+ * Returns the decimal string for the 2's complement hexadecimal string.
+ * @function
+ * @param {string} hex
+ * @param {number} bits - 8, 16, 32, 64, or 128
+ */
+exports.decify = (function() {
+  var two = Big('2');
+
+  return function decify(hex, bits, signed) {
+    var m = two.pow(bits);
+    var firstNeg = two.pow(bits - 1);
+    var unsigned = hex
+      .toUpperCase()
+      .split('')
+      .reverse()
+      .map(valueOfHexAtPosition)
+      .reduce(sum)
+      .mod(m);
+
+    //is negative
+    if (signed && unsigned.cmp(firstNeg) >= 0) {
+      return unsigned.minus(m).toFixed();
+    }
+    return unsigned.toFixed();
+  };
+})();
+
+var sum = function(a, b) {
+  return b.plus(a);
+};
+
+var valueOfHexAtPosition = (function() {
+  var sexa = Big('16');
+  var h2d = {
+    '0': '0',
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    'A': '10',
+    'B': '11',
+    'C': '12',
+    'D': '13',
+    'E': '14',
+    'F': '15',
+  };
+
+  return function valueOfHexAtPosition(h, pos) {
+    return Big(h2d[h]).times(sexa.pow(pos));
+  };
+})();
---
+++ test/index.js
@@ -0,0 +1,50 @@
+var expect = require('chai').expect;
+var hexa = require('../');
+
+describe('hexify', function() {
+  [
+    //decimal, bits, hex
+    ['1', 64, '0000000000000001'],
+    ['01', 8, '01'],
+    ['-1', 8, 'FF'],
+    ['-1', 16, 'FFFF'],
+    ['-1', 32, 'FFFFFFFF'],
+    ['-1', 64, 'FFFFFFFFFFFFFFFF'],
+    ['-1', 128, 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'],
+    ['10', 64, '000000000000000A'],
+    ['689', 16, '02B1'],
+    ['689', 8, 'B1'],
+    ['256', 8, '00'],
+    ['24197857200151252744792662746087043600', 128, '1234567890ABCDEFEDCBA09876543210'],
+    ['-129', 8, '7F'],
+    ['-128', 8, '80'],
+    ['-129', 16, 'FF7F'],
+  ].forEach(function(t, i) {
+    it(['("', t[0], '", ', t[1], ') => "', t[2], '"'].join(''), function() {
+      expect(hexa.hexify(t[0], t[1])).to.equal(t[2]);
+    });
+  });
+});
+
+describe('decify', function() {
+  [
+    //hex, bits, signed flag, decimal
+    ['0', 8, true, '0'],
+    ['00', 8, false, '0'],
+    ['FF', 8, true, '-1'],
+    ['FF', 8, false, '255'],
+    ['FFFF', 8, true, '-1'],
+    ['FFFF', 8, false, '255'],
+    ['000000000000000A', 64, false, '10'],
+    ['7F', 8, true, '127'],
+    ['80', 8, true, '-128'],
+    ['80', 8, false, '128'],
+    ['FF7F', 16, true, '-129'],
+    ['1234567890ABCDEFEDCBA09876543210', 128, true, '24197857200151252744792662746087043600'],
+    ['1234567890ABCDEFEDCBA09876543210', 128, false, '24197857200151252744792662746087043600'],
+  ].forEach(function(t, i) {
+    it(['("', t[0], '", ', t[1], ', ', t[2], ') => "', t[3], '"'].join(''), function() {
+      expect(hexa.decify(t[0], t[1], t[2])).to.equal(t[3]);
+    });
+  });
+});
